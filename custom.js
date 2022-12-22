51c64f0e6baa4eed73cdec02a5b6f82229f2e523
Yvan Oliveira <85844516+yvanoliveira@users.noreply.github.com>
2022-12-22 15:13:24 -0300

    Create custom.js

---
+++ custom.js
@@ -0,0 +1,8 @@
+// Hides the edit app button.
+$('#appmode-leave').hide();
+
+// Hides the kernel busy indicator.
+$('#appmode-busy').hide();
+
+// Adds a loading message.
+$('#appmode-loader').append('<h2>Loading...</h2>');
